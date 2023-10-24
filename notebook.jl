1868daa806a60599d939eb53eee38a5f5bbdef4b
Jorge Peña <jorge.pena@iast.fr>
2023-10-24 15:58:32 +0200

    revised code for resubmission at TPB

--- notebook.jl
+++ notebook.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.9
+# v0.19.27
 
 using Markdown
 using InteractiveUtils
@@ -17,9 +17,6 @@ begin
 	using Plots
 end
 
-# ╔═╡ 8515b2c8-d3a9-11ed-10c4-b77658263370
-
-
 # ╔═╡ 196443bd-913a-483a-bd57-0af0ca9f29df
 md"## Libraries"
 
@@ -33,22 +30,31 @@ md"## Figures"
 md"### Figure 1"
 
 # ╔═╡ 92e2b9aa-c89d-4504-ba77-bb2a82b7259e
-pl_pivot_dynamics = SD.plot_pivot_dynamics(2,4)
+pl_Fig1 = SD.plot_pivot_dynamics(2,4)
 
 # ╔═╡ 84e50aec-cc65-4fa4-8238-e0916fbdc9e9
-savefig(pl_pivot_dynamics,"./figs/fig_dynamics.pdf")
+savefig(pl_Fig1,"./figs/Fig1.pdf")
 
 # ╔═╡ 4a645257-3c19-4bad-9148-8fcbc2dd3d2b
 md"### Figure 2"
 
 # ╔═╡ 1d292187-b726-4306-af6b-844a3043bf4d
-pl_critical_costs = SD.plot_critical_costs(30,30)
+pl_Fig2 = SD.plot_critical_costs(30,30)
 
 # ╔═╡ 9787cc02-5fc6-4ed2-8b84-d5982a6cbe9e
-savefig(pl_critical_costs,"./figs/fig_costs.pdf")
+savefig(pl_Fig2,"./figs/Fig2.pdf")
 
 # ╔═╡ c317fb8b-c845-4682-803a-540dd7af0fdb
 md"### Figure 3"
+
+# ╔═╡ 2fb260c4-daf6-4642-969d-ee62eb520a70
+pl_Fig3 = SD.plot_group_size_effect_on_pivot_probability(2,4,.2)
+
+# ╔═╡ 5336d2ef-0dc9-443a-ac06-98408f3cbab4
+savefig(pl_Fig3,"./figs/Fig3.pdf")
+
+# ╔═╡ b26fd8cf-bdf6-460f-bfa3-ef3bb1e75ffe
+md"### Figure 4"
 
 # ╔═╡ 5b75cdcf-233f-45df-aee8-a655be1f7323
 pl_p = SD.plot_p(30,0.2)
@@ -56,56 +62,65 @@ pl_p = SD.plot_p(30,0.2)
 # ╔═╡ dfb0e5f1-02b9-4ec2-9b44-2cc165cf607e
 pl_u = SD.plot_u(30,0.2)
 
-# ╔═╡ e8d17680-c6dc-41cf-9761-79bbe0cebdc7
-pl_phi = SD.plot_ϕ(30,0.2)
+# ╔═╡ ab8234bf-3708-4848-b73c-9946a5e58cdf
+pl_Fig4=plot(pl_p,pl_u,layout=(1,2),size=(600,300))
 
-# ╔═╡ 616a210d-fa75-492a-8c53-67a4e1842346
-pl_phi_2 = SD.plot_ϕ_2()
-
-# ╔═╡ 3fa8b0c7-dc5a-4a19-a5fb-79a0b56e830a
-pl_Fig_3=plot(pl_p,pl_u,pl_phi,pl_phi_2,layout=(2,2))
-
-# ╔═╡ 6cddb256-2e32-473b-ad7f-8b1ce4978a62
-savefig(pl_Fig_3,"./figs/fig_p_phi_u.pdf")
+# ╔═╡ ce015360-c632-4285-9630-82557f0e5b5b
+savefig(pl_Fig4,"./figs/Fig4.pdf")
 
 # ╔═╡ 8aa82202-c557-448a-a392-3584f3e7df73
-md"### Figure 4"
-
-# ╔═╡ 83974d99-5102-4b00-b485-0562fd36ebdc
-pl_p_heatmap = SD.plot_p_heatmap(30)
-
-# ╔═╡ 3c37bb93-e768-4b16-84e6-4d451387f7ab
-savefig(pl_p_heatmap,"./figs/fig_p.pdf")
-
-# ╔═╡ 39adac8e-9253-422a-a798-16f70634dd2d
 md"### Figure 5"
 
-# ╔═╡ c8bee348-a463-46fd-8052-6ec9377280eb
-pl_basin_heatmap = SD.plot_basin_heatmap(30)
+# ╔═╡ 83974d99-5102-4b00-b485-0562fd36ebdc
+pl_Fig5 = SD.plot_p_heatmap(30)
 
-# ╔═╡ 14a93dff-236f-4663-a1c2-35e664549e77
-savefig(pl_basin_heatmap,"./figs/fig_basin.pdf")
+# ╔═╡ 3c37bb93-e768-4b16-84e6-4d451387f7ab
+savefig(pl_Fig5,"./figs/Fig5.pdf")
 
-# ╔═╡ 03a503fd-f0bd-414c-ba22-a3332c578f83
+# ╔═╡ 39adac8e-9253-422a-a798-16f70634dd2d
 md"### Figure 6"
 
-# ╔═╡ 381274cb-96a0-4d5b-bffa-df199151103c
-pl_u_heatmap = SD.plot_u_heatmap(30)
+# ╔═╡ c8bee348-a463-46fd-8052-6ec9377280eb
+pl_Fig6 = SD.plot_basin_heatmap(30)
 
-# ╔═╡ 07a7ac57-08f0-4180-9890-c069cde116e1
-savefig(pl_u_heatmap,"./figs/fig_u.pdf")
+# ╔═╡ 14a93dff-236f-4663-a1c2-35e664549e77
+savefig(pl_Fig6,"./figs/Fig6.pdf")
 
-# ╔═╡ 51b1a822-839d-4ded-a866-9d195941cc51
+# ╔═╡ 03a503fd-f0bd-414c-ba22-a3332c578f83
 md"### Figure 7"
 
+# ╔═╡ 381274cb-96a0-4d5b-bffa-df199151103c
+pl_Fig7 = SD.plot_u_heatmap(30)
+
+# ╔═╡ 07a7ac57-08f0-4180-9890-c069cde116e1
+savefig(pl_Fig7,"./figs/Fig7.pdf")
+
+# ╔═╡ ddfd4e62-4144-408e-a910-c04e81172c23
+md"### Figure 8"
+
+# ╔═╡ 100e204e-1dab-4c33-bcfb-5f41c66aee36
+pl_phi = SD.plot_ϕ(30,0.2)
+
+# ╔═╡ 5ef6a226-cca1-45ad-a5dd-c06f2dcf1389
+pl_phi_2 = SD.plot_ϕ_2()
+
+# ╔═╡ 0d05cf93-0e5f-4919-9cd7-29afd6363e2d
+pl_Fig8=plot(pl_phi_2,pl_phi,layout=(1,2),size=(600,300))
+
+# ╔═╡ 024de5f2-2488-466c-baf8-3673440a7369
+savefig(pl_Fig8,"./figs/Fig8.pdf")
+
+# ╔═╡ 51b1a822-839d-4ded-a866-9d195941cc51
+md"### Figure 9"
+
 # ╔═╡ d1681659-ceab-42b5-b461-24d7ae299381
-pl_phi_heatmap = SD.plot_ϕ_heatmap(30)
+pl_Fig9 = SD.plot_ϕ_heatmap(30)
 
 # ╔═╡ 0b0a6317-2253-4b6b-a089-b33efe7682c1
-savefig(pl_phi_heatmap,"./figs/fig_phi.pdf")
+savefig(pl_Fig9,"./figs/fig_phi.pdf")
 
 # ╔═╡ 83b162d8-d17c-43c7-ae48-31fce0f6faf3
-md"### Figure 8"
+md"### Figure 10"
 
 # ╔═╡ 03265d6e-71f0-48a9-a1a9-235de8579c7c
 pl_pivot_Poisson = SD.plot_pivot_Poisson(2,0.2)
@@ -120,31 +135,31 @@ pl_limit_u = SD.plot_limit_u()
 pl_limit_phi = SD.plot_limit_ϕ()
 
 # ╔═╡ 2304c10b-5408-43cb-959f-a18bbef6cbc8
-pl_limit=plot(pl_pivot_Poisson,pl_limit_p,pl_limit_u,pl_limit_phi,layout=(2,2))
+pl_Fig10=plot(pl_pivot_Poisson,pl_limit_p,pl_limit_u,pl_limit_phi,layout=(2,2))
 
 # ╔═╡ 46c44f40-b9dc-444d-8afb-7f41685c0a60
-savefig(pl_limit,"./figs/fig_limit.pdf")
+savefig(pl_Fig10,"./figs/Fig10.pdf")
 
 # ╔═╡ Cell order:
-# ╠═8515b2c8-d3a9-11ed-10c4-b77658263370
 # ╠═196443bd-913a-483a-bd57-0af0ca9f29df
 # ╠═0ed771d2-d325-4c42-b7d9-333897de126e
 # ╠═a1d5edf6-7cfe-4f07-b520-bad358b70876
 # ╠═05db2254-1f29-4c17-8547-dfc8cec0b456
 # ╟─dfbf4914-34cc-483b-94bc-dd39c6633593
-# ╠═4ba3da33-2d99-4dba-9c96-6076833b5f65
+# ╟─4ba3da33-2d99-4dba-9c96-6076833b5f65
 # ╠═92e2b9aa-c89d-4504-ba77-bb2a82b7259e
 # ╠═84e50aec-cc65-4fa4-8238-e0916fbdc9e9
 # ╠═4a645257-3c19-4bad-9148-8fcbc2dd3d2b
 # ╠═1d292187-b726-4306-af6b-844a3043bf4d
 # ╠═9787cc02-5fc6-4ed2-8b84-d5982a6cbe9e
-# ╟─c317fb8b-c845-4682-803a-540dd7af0fdb
+# ╠═c317fb8b-c845-4682-803a-540dd7af0fdb
+# ╠═2fb260c4-daf6-4642-969d-ee62eb520a70
+# ╠═5336d2ef-0dc9-443a-ac06-98408f3cbab4
+# ╠═b26fd8cf-bdf6-460f-bfa3-ef3bb1e75ffe
 # ╠═5b75cdcf-233f-45df-aee8-a655be1f7323
 # ╠═dfb0e5f1-02b9-4ec2-9b44-2cc165cf607e
-# ╠═e8d17680-c6dc-41cf-9761-79bbe0cebdc7
-# ╠═616a210d-fa75-492a-8c53-67a4e1842346
-# ╠═3fa8b0c7-dc5a-4a19-a5fb-79a0b56e830a
-# ╠═6cddb256-2e32-473b-ad7f-8b1ce4978a62
+# ╠═ab8234bf-3708-4848-b73c-9946a5e58cdf
+# ╠═ce015360-c632-4285-9630-82557f0e5b5b
 # ╠═8aa82202-c557-448a-a392-3584f3e7df73
 # ╠═83974d99-5102-4b00-b485-0562fd36ebdc
 # ╠═3c37bb93-e768-4b16-84e6-4d451387f7ab
@@ -154,6 +169,11 @@ savefig(pl_limit,"./figs/fig_limit.pdf")
 # ╠═03a503fd-f0bd-414c-ba22-a3332c578f83
 # ╠═381274cb-96a0-4d5b-bffa-df199151103c
 # ╠═07a7ac57-08f0-4180-9890-c069cde116e1
+# ╠═ddfd4e62-4144-408e-a910-c04e81172c23
+# ╠═100e204e-1dab-4c33-bcfb-5f41c66aee36
+# ╠═5ef6a226-cca1-45ad-a5dd-c06f2dcf1389
+# ╠═0d05cf93-0e5f-4919-9cd7-29afd6363e2d
+# ╠═024de5f2-2488-466c-baf8-3673440a7369
 # ╠═51b1a822-839d-4ded-a866-9d195941cc51
 # ╠═d1681659-ceab-42b5-b461-24d7ae299381
 # ╠═0b0a6317-2253-4b6b-a089-b33efe7682c1
--- src/ShirkersDilemma.jl
+++ src/ShirkersDilemma.jl
@@ -5,30 +5,37 @@ using Plots, LaTeXStrings, ColorSchemes
 
 greet() = print("Hello World!")
 
-# probability that there are at most ζ shirkers in a group (Eq. 1)
+### Preliminary functions ###
+
+# probability that there are at most ζ shirkers in a group (Eq. 2)
 Πζn(q,ζ,n) = Distributions.cdf(Binomial(n,q),ζ)
 
-πζn(q,ζ,n) = Distributions.pdf(Binomial(n-1,q),ζ) # pivot probability (Eq. 6)
+# pivot probability (Eq. 7)
+πζn(q,ζ,n) = Distributions.pdf(Binomial(n-1,q),ζ)
 
-cbar(ζ,n) = πζn(ζ/(n-1),ζ,n) # critical cost (Eq. 8)
+# critical cost (Eq. 9)
+cbar(ζ,n) = πζn(ζ/(n-1),ζ,n)
 
-cdagger(ζ) = (ζ/(ζ+1))^ζ # maximum critical cost (Eq. 14)
+# maximum critical cost (Eq. 15)
+cdagger(ζ) = (ζ/(ζ+1))^ζ 
 
-ρ(k,λ) = Distributions.pdf(Poisson(λ),k) # probability mass function of a Poisson distribution with parameter λ (Eq. 16)
+# probability mass function of a Poisson distribution with parameter λ (Eq. 17)
+ρ(k,λ) = Distributions.pdf(Poisson(λ),k)
 
-cbarstar(ζ) = ρ(ζ,ζ) # limit critical cost (Eq. 15)
+# limit critical cost (Eq. 16)
+cbarstar(ζ) = ρ(ζ,ζ)
 
-# unstable interior rest point (rightmost solution of Eq. 7)
+# unstable interior rest point (larger solution of the pivotality condition, Eq. 8)
 function quζc(n,ζ,c)
     if c >= cbar(ζ,n)
         return NaN
     else
-        g(q) = c - πζn(q,ζ,n) # gain function (Eq. 5)
+        g(q) = c - πζn(q,ζ,n) # gain function (Eq. 6)
         return find_zero(g,(ζ/(n-1),1))
     end
 end
 
-# stable interior rest point (leftmost solution of Eq. 7)
+# stable interior rest point (smaler solution of the pivotality condition, Eq. 8)
 function qsζc(n,ζ,c)
     if c >= cbar(ζ,n)
         return NaN
@@ -38,7 +45,7 @@ function qsζc(n,ζ,c)
     end
 end
 
-# minimal rest point (Eq. 9)
+# minimal rest point (Eq. 10)
 function qζc(n,ζ,c)
     if c >= cbar(ζ,n)
         return 1.
@@ -47,22 +54,23 @@ function qζc(n,ζ,c)
     end
 end
 
-# proportion of volunteers at the minimal rest point (Eq. 10)
+# proportion of volunteers at the minimal rest point (Eq. 11)
 pζc(n,ζ,c) = 1 - qζc(n,ζ,c)
 
-# expected payoff at the minimal rest point (Eq. 11)
+# expected payoff at the minimal rest point (Eq. 13)
 uζc(n,ζ,c) = Πζn(qζc(n,ζ,c),ζ-1,n-1)
 
 # success probability at the minimal rest point (Eq. 12)
 ϕζc(n,ζ,c) = Πζn(qζc(n,ζ,c),ζ,n)
 
-# expected number of co-players that are shirkers at the minimal rest point (Eq. 26)
+# expected number of co-players that are shirkers at the minimal rest point (first eq. in appendix A.5)
 λζc(n,ζ,c) = (n-1)*qζc(n,ζ,c)
 
-# expected number of group members that are shirkers at the minimal rest point
+# expected number of group members that are shirkers at the minimal rest point (Eq. 24)
 μζc(n,ζ,c) = n*qζc(n,ζ,c)
 
 # limit expected number of co-players that are shirkers at the minimal rest point
+# (second eq. in appendix A.5, see also Lemma 4)
 function λζcstar(ζ,c)
     if c > cbarstar(ζ)
         return Inf
@@ -73,12 +81,14 @@ function λζcstar(ζ,c)
 end
 
 # limit expected number of group members that are shirkers at the minimal rest point
+# (see Lemma 4)
 μζcstar(ζ,c) = λζcstar(ζ,c)
 
 # cumulative distribution function of a Poisson distribution with parameter λ
+# (second eq. in Proposition 4)
 P(k,λ) = Distributions.cdf(Poisson(λ),k)
 
-# limit proportion of volunteers at the minimal rest point (Eq. 28)
+# limit proportion of volunteers at the minimal rest point (Eq. 26)
 function pζcstar(ζ,c)
 	if c > cbarstar(ζ)
         return 0.
@@ -87,7 +97,7 @@ function pζcstar(ζ,c)
 	end
 end
 
-# limit expected payoff at the minimal rest point (Eq. 28)
+# limit expected payoff at the minimal rest point (Eq. 26, see also appendix A.5)
 function uζcstar(ζ,c)
 	if c > cbarstar(ζ)
 		return 0.
@@ -96,7 +106,7 @@ function uζcstar(ζ,c)
 	end
 end
 
-# limit success probability at the minimal rest point (Eq. 28)
+# limit success probability at the minimal rest point (Eq. 26, see also appendix A.5)
 function ϕζcstar(ζ,c)
 	if c > cbarstar(ζ)
         return 0.
@@ -104,6 +114,8 @@ function ϕζcstar(ζ,c)
 		return P(ζ,μζcstar(ζ,c))
 	end
 end
+
+### Figures ###
 
 # colors
 colorname(c) = string("#",hex(c))
@@ -116,7 +128,7 @@ pink = colorname(ColorSchemes.seaborn_colorblind[7])
 gray = colorname(ColorSchemes.seaborn_colorblind[8])
 markershapes = [:circle, :rect, :star5, :diamond, :hexagon, :cross, :xcross, :utriangle, :dtriangle, :rtriangle]
 
-# Figure 1 (pivot probability and evolutionary dynamics)
+# Fig. 1 (pivot probability and evolutionary dynamics)
 function plot_pivot_dynamics(ζ,n)
 
     eps_arrows = 0.03
@@ -202,11 +214,13 @@ function plot_pivot_dynamics(ζ,n)
 
 	plot!(subplot=4,xlabel=L"\textrm{proportion \ of \ shirkers,} \ q",xlabelfontsize=8)
 
+	plot!(size=(600,400))
+
     return pl_pivot_dynamics
 	
 end
 
-# Figure 2 (critical costs)
+# Fig. 2 (critical costs)
 function plot_critical_costs(nmax,ζmax)
 
 	pl_critical_costs = plot(layout=(1,2))
@@ -227,7 +241,6 @@ function plot_critical_costs(nmax,ζmax)
 	plot!(subplot=1,ylabel=L"\textrm{cost,} \ c")
 
 	# subplot 2
-
 	plot!(subplot=2,legend=:right,ylims=(0,.52),xaxis=:log)
 
 	ζs = range(1,stop=ζmax)
@@ -241,73 +254,109 @@ function plot_critical_costs(nmax,ζmax)
 	plot!(subplot=2,xticks=([1,5,10,30],[1,5,10,30]))
 	
 	plot!(subplot=2,xlabel=L"\textrm{threshold,} \ \zeta")
+
+	plot!(size=(600,300))
     
     return pl_critical_costs
 	
 end
 
-# Figure 3 top left (proportion of volunteers as a function of group size)
+# Fig. 3 (illustration of Lemma 3; group size effects on pivot probabilities)
+function plot_group_size_effect_on_pivot_probability(ζ,n,c)
+
+	q = range(0,stop=1,length=1000)
+	y = πζn.(q,ζ,n)
+	qhat = ζ/(n-1)
+	cb = cbar(ζ,n)
+	qs = qζc(n,ζ,c)
+	qu = quζc(n,ζ,c)
+
+	yy = πζn.(q,ζ,n+1)
+	qqhat = ζ/n
+	ccb = cbar(ζ,n+1)
+	qqs = qζc(n+1,ζ,c)
+	qqu = quζc(n+1,ζ,c)
+
+	pl_pivots = plot(layout=(1,1),framestyle=:box,xlims=(-0.05,1.05),ylims=(-0.05,1.05))
+
+	# horizontal line cost
+	plot!([0; 1],[c; c],color=gray,linewidth=3,label=false)
+	 
+	# horizontal line critical cost n
+	plot!([0; qhat],[cb; cb],color=gray,linestyle=:dash,linewidth=2,label=false)
+
+	# vertical line critical cost n
+	plot!([qhat; qhat],[0; cb],color=gray,linestyle=:dash,linewidth=2,label=false)
+
+	# pivot probability n
+	plot!(q,y,color=blue,linestyle=:dash,linewidth=3,label=L"\ \pi_{\zeta,n}(q)")
+
+	# stable interior rest point n
+	plot!([qs],[c],markershape=:circle,markersize=10,color=red,alpha=0.5,markerstrokecolor=red,label=false)
+
+	#  unstable interior rest point n
+	plot!([qu],[c],markershape=:circle,markersize=10,alpha=0.5,color=red,markerstrokecolor=red,label=false)
+	plot!([qu],[c],markershape=:circle,markersize=7,alpha=1.0,color="white",markerstrokecolor="white",label=false)
+
+	# pivot probability n+1
+	plot!(q,yy,color=blue,linewidth=3,label=L"\ \pi_{\zeta,n+1}(q)")
+
+	# horizontal line critical cost n
+	plot!([0; qqhat],[ccb; ccb],color=gray,linestyle=:dash,linewidth=2,label=false)
+
+	# vertical line critical cost n
+	plot!([qqhat; qqhat],[0; ccb],color=gray,linestyle=:dash,linewidth=2,label=false)
+
+	# stable interior rest point n+1
+	plot!([qqs],[c],markershape=:circle,markersize=10,color=red,markerstrokecolor=red,label=false)
+
+	#  unstable interior rest point n+1
+	plot!([qqu],[c],markershape=:circle,markersize=10,color=red,markerstrokecolor=red,label=false)
+	plot!([qqu],[c],markershape=:circle,markersize=7,color="white",markerstrokecolor="white",label=false)
+
+	plot!(xrotation=90,xticks=([0,qhat,qqhat,qs,qqs,qu,qqu,1], [L"0",L"\frac{\zeta}{n-1}",L"\frac{\zeta}{n}",L"q^s_{\zeta,c}(n)",L"q^s_{\zeta,c}(n+1)",L"q^u_{\zeta,c}(n)",L"q^u_{\zeta,c}(n+1)",L"1"]))
+	plot!(yticks = ([0,c,cb,ccb,1], [L"0",L"c",L"\bar{c}_{\zeta,n}",L"\bar{c}_{\zeta,n+1}",L"1"]))
+	
+	plot!(xlabel=L"\textrm{proportion \ of \ shirkers,} \ q")
+	plot!(ylabel=L"\textrm{pivot \ probability}")
+
+	plot!(framestyle=:box) # box around plot
+	plot!(xlabelfontsize=10,ylabelfontsize=10,tickfontsize=10,legendfontsize=10,bottom_margin=5Plots.mm)
+
+	plot!(size=(600,400))
+
+	return pl_pivots
+end
+
+# Fig. 4, left panel (proportion of volunteers as a function of group size)
 function plot_p(nmax,c)
 	pl_p = plot(legend=:right)
-	#c_p = 0.2
-	#nmax_p=30
-	for ζ in 1:10
+	for ζ in 1:5
 		n = range(ζ+2,stop=nmax)
 		p = pζc.(n,ζ,c)
 		color = colorname(ColorSchemes.seaborn_colorblind[ζ])
-		plot!(n,p,markershape=markershapes[ζ],markersize=3,legendfontsize=5,color=color,label="\$\\zeta=$ζ\$")
+		plot!(n,p,ylims=(-0.05,1.0),markershape=markershapes[ζ],markersize=3,legend=:bottomleft,legendfontsize=5,color=color,label="\$\\zeta=$ζ\$")
 	end
-	plot!(ylabel=L"\textrm{proportion \ of \ volunteers,} \ p_{\zeta,c}(n)",ylabelfontsize=8)
+	plot!(xlabel=L"\textrm{group \ size,} \ n") 
+	plot!(ylabel=L"\textrm{proportion \ of \ volunteers,} \ p_{\zeta,c}(n)")
     return pl_p
 end
 
-# Figure 3 top right (expected payoff as a function of group size)
+# Fig. 4, right panel (expected payoff as a function of group size)
 function plot_u(nmax,c)
 	pl_u = plot(legend=:bottomleft)
-	for ζ in 1:10
+	for ζ in 1:5
 		n = range(ζ+2,stop=nmax)
 		u = uζc.(n,ζ,c)
-		us = uζcstar(ζ,c)
 		color = colorname(ColorSchemes.seaborn_colorblind[ζ])
-		plot!(n,u,markershape=markershapes[ζ],markersize=3,legendfontsize=5,color=color,label="\$\\zeta=$ζ\$")
+		plot!(n,u,ylims=(-0.05,1.0),markershape=markershapes[ζ],markersize=3,legend=false,color=color,label="\$\\zeta=$ζ\$")
 	end
-	plot!(ylabel=L"\textrm{expected \ payoff,} \ u_{\zeta,c}(n)",ylabelfontsize=8)
+	plot!(xlabel=L"\textrm{group \ size,} \ n") 
+	plot!(ylabel=L"\textrm{expected \ payoff,} \ u_{\zeta,c}(n)")
     return pl_u
 end
 
-# Figure 3 bottom left (success probability as a function of group size)
-function plot_ϕ(nmax,c)
-	pl_phi = plot(legend=:bottomleft)
-	for ζ in 1:10
-		n=range(ζ+2,stop=nmax)
-		ϕ = ϕζc.(n,ζ,c)
-		ϕs = ϕζcstar(ζ,c)
-		color = colorname(ColorSchemes.seaborn_colorblind[ζ])
-		plot!(n,ϕ,markershape=markershapes[ζ],markersize=3,legendfontsize=5,color=color,label="\$\\zeta=$ζ\$")
-	end
-	plot!(xlabel=L"\textrm{group \ size,} \ n") 
-	plot!(ylabel=L"\textrm{success \ probability,} \phi_{\zeta,c}(n)",ylabelfontsize=8)
-	plot!(markersize=1)
-    return pl_phi
-end
-
-# Figure 3 bottom right (success probability as a function of group size)
-function plot_ϕ_2()
-	pl_phi_2 = plot(legend=:right)
-	ns = range(3,stop=10)
-	cs = [0.3, 0.3075, 0.31]
-	for i in 1:length(cs)
-		c = cs[i]
-		ϕ = ϕζc.(ns,1,c)
-		color = colorname(ColorSchemes.seaborn_colorblind[i])
-		plot!(ns,ϕ,markershape=markershapes[i],markersize=3,legendfontsize=5,color=color,label="\$c=$c\$")
-	end
-	plot!(xlabel=L"\textrm{group \ size,} \ n") 
-	plot!(ylabel=L"\textrm{success \ probability,} \phi_{1,c}(n)",ylabelfontsize=8)
-    return pl_phi_2
-end
-
-# Figure 4 (heatmap proportion of volunteers)
+# Fig. 5 (heatmap proportion of volunteers)
 function plot_p_heatmap(nmax)
 	pl_p_heatmap = plot(layout=(2,2))
 	color_heatmap =:viridis
@@ -331,7 +380,7 @@ function plot_p_heatmap(nmax)
     return pl_p_heatmap 
 end
 
-# Figure 5 (heatmap proportion of volunteers)
+# Fig. 6 (heatmap basin of attraction stable equilibrium)
 function plot_basin_heatmap(nmax)
     pl_basin_heatmap = plot(layout=(2,2))
 	color_heatmap = :viridis
@@ -354,7 +403,7 @@ function plot_basin_heatmap(nmax)
     return pl_basin_heatmap 	
 end
 
-# Figure 6 (heatmap expected payoff)
+# Fig. 7 (heatmap expected payoff)
 function plot_u_heatmap(nmax)
 	pl_u_heatmap = plot(layout=(2,2))
 	color_heatmap = :viridis
@@ -378,7 +427,38 @@ function plot_u_heatmap(nmax)
     return pl_u_heatmap 	 	
 end
 
-# Figure 7 (heatmap success probability)
+# Fig. 8, right panel (success probability as a function of group size)
+function plot_ϕ(nmax,c)
+	pl_phi = plot(legend=:bottomleft)
+	for ζ in 1:5
+		n=range(ζ+2,stop=nmax)
+		ϕ = ϕζc.(n,ζ,c)
+		color = colorname(ColorSchemes.seaborn_colorblind[ζ])
+		plot!(n,ϕ,markershape=markershapes[ζ],markersize=3,legendfontsize=5,color=color,label="\$\\zeta=$ζ\$")
+	end
+	plot!(xlabel=L"\textrm{group \ size,} \ n") 
+	plot!(ylabel=L"\textrm{success \ probability,} \phi_{\zeta,c}(n)")
+	plot!(markersize=1)
+    return pl_phi
+end
+
+# Fig. 8, left panel (success probability as a function of group size)
+function plot_ϕ_2()
+	pl_phi_2 = plot()
+	ns = range(3,stop=10)
+	cs = [0.305, 0.3075, 0.31]
+	for i in 1:length(cs)
+		c = cs[i]
+		ϕ = ϕζc.(ns,1,c)
+		color = colorname(ColorSchemes.seaborn_colorblind[i])
+		plot!(ns,ϕ,markershape=markershapes[i],markersize=3,legend=:topright,legendfontsize=5,color=color,label="\$c=$c\$")
+	end
+	plot!(xlabel=L"\textrm{group \ size,} \ n") 
+	plot!(ylabel=L"\textrm{success \ probability,} \phi_{1,c}(n)")
+    return pl_phi_2
+end
+
+# Fig. 9 (heatmap success probability)
 function plot_ϕ_heatmap(nmax)
 	pl_phi_heatmap = plot(layout=(2,2),link=:all)
 	color_heatmap = :viridis
@@ -402,7 +482,7 @@ function plot_ϕ_heatmap(nmax)
     return pl_phi_heatmap  	
 end
 
-# Figure 8 top left (poisson pivot probability)
+# Fig. 10, top left (poisson pivot probability)
 function plot_pivot_Poisson(ζ,c)
 
 	pl_pivot_Poisson = plot(framestyle=:box,ylims=(0,1))
@@ -411,7 +491,7 @@ function plot_pivot_Poisson(ζ,c)
 	λs = range(0,λmax,length=1000)
 	ρs = ρ.(ζ,λs)
 	phis_Poisson = P.(ζ,λs)
-	us_Poisson = P.(ζ-1,λs)
+	us_Poisson = P.(ζ,λs) .- c
 	
 	plot!(λs,ρs,color=blue,linewidth=3,label="\$\\rho_{\\zeta}(\\lambda)\$") # pivot probability
 	plot!([0;ζ],[cbarstar(ζ);cbarstar(ζ)],color=blue,linewidth=2,linestyle=:dash,label=false)
@@ -421,7 +501,7 @@ function plot_pivot_Poisson(ζ,c)
 	plot!([μζcstar(ζ,c);μζcstar(ζ,c)],[0;c],color=gray,linewidth=2,linestyle=:dash,label=false)
 	plot!([μζcstar(ζ,c)],[c],markersize=8,color=blue,markerstrokecolor=blue,markershape=:circle,label=false)
 
-	plot!(λs,us_Poisson,color=red,linewidth=3,label="\$P_{\\zeta-1}(\\lambda)\$")
+	plot!(λs,us_Poisson,color=red,linewidth=3,label="\$P_{\\zeta}(\\lambda) - c\$")
 	plot!([μζcstar(ζ,c);μζcstar(ζ,c)],[0;uζcstar(ζ,c)],color=gray,linewidth=2,linestyle=:dash,label=false)
 	plot!([0;μζcstar(ζ,c)],[uζcstar(ζ,c);uζcstar(ζ,c)],color=red,linewidth=2,linestyle=:dash,label=false)	
 
@@ -429,7 +509,7 @@ function plot_pivot_Poisson(ζ,c)
 	plot!([μζcstar(ζ,c);μζcstar(ζ,c)],[0;ϕζcstar(ζ,c)],color=gray,linewidth=2,linestyle=:dash,label=false)
 	plot!([0;μζcstar(ζ,c)],[ϕζcstar(ζ,c);ϕζcstar(ζ,c)],color=green,linewidth=2,linestyle=:dash,label=false)
 	
-	plot!(xticks=([0,μζcstar(ζ,c),ζ],[L"0",L"\mu_{\zeta,c}^*=\lambda_{\zeta,c}^*",L"\zeta"]))
+	plot!(xticks=([0,μζcstar(ζ,c),ζ],[L"0",L"\mu_{\zeta,c}^*",L"\zeta"]))
 	plot!(yticks=([0,cbarstar(ζ),uζcstar(ζ,c),ϕζcstar(ζ,c),1], [L"0", L"\bar{c}_{\zeta}^*",L"u_{\zeta,c}^*",L"\phi_{\zeta,c}^*",L"1"]))	
 	
 	plot!(xlabel=L"\textrm{expected \ number \ of \ (other) \ shirkers}",xlabelfontsize=8)	
@@ -439,7 +519,7 @@ function plot_pivot_Poisson(ζ,c)
 	
 end
 
-# Figure 8 top right (limit proportion of volunteers)
+# Fig. 10 top right (limit proportion of volunteers)
 function plot_limit_p()
 	pl_limit_p = plot(ylims=(0.,1.05))
 	cs_limits = range(eps(),0.5-eps(),length=1500)
@@ -456,7 +536,7 @@ function plot_limit_p()
     return pl_limit_p
 end
 
-# Figure 8 bottom left (limit expected payoff)
+# Fig. 10 bottom left (limit expected payoff)
 function plot_limit_u()
 	pl_limit_u = plot(ylims=(0.,1.05))
     cs_limits = range(eps(),0.5-eps(),length=1500)
@@ -473,7 +553,7 @@ function plot_limit_u()
     return pl_limit_u
 end
 
-# Figure 8 bottom right (limit success probability)
+# Fig. 10 bottom right (limit success probability)
 function plot_limit_ϕ()
 	pl_limit_phi = plot(ylims=(0.,1.05))
     cs_limits = range(eps(),0.5-eps(),length=1500)
